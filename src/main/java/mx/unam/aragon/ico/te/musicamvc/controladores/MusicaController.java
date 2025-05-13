ea2bb81c368ea6a2a0fd9b3d592efa4f85096e77
jeshc <hcjesus@gmail.com>
2025-05-13 12:03:44 -0600

    Front para alta de artita completo

--- src/main/java/mx/unam/aragon/ico/te/musicamvc/controladores/MusicaController.java
+++ src/main/java/mx/unam/aragon/ico/te/musicamvc/controladores/MusicaController.java
@@ -1,10 +1,10 @@
 package mx.unam.aragon.ico.te.musicamvc.controladores;
 
 import mx.unam.aragon.ico.te.musicamvc.modelos.Artista;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/tienda")
@@ -17,9 +17,24 @@ public class MusicaController {
 
     @GetMapping("/artista/")
     public String artista(Model model) {
-        Artista artista = new Artista("Algo",25,"Rock",1,"https://queen.fandom.com/es/wiki/Freddie_Mercury?file=IMG-20151114-WA0005.jpg", "Freddie Mercury");
+        Artista artista = new Artista("Algo",25,"Rock",1,"https://upload.wikimedia.org/wikipedia/commons/9/96/Freddie_Mercury_%281977_Press_Kit_Photo%29.jpg", "Freddie Mercury");
         model.addAttribute("artista", artista);
         return "artista";
+    }
+
+    @GetMapping("/nuevo")
+    public String nuevo(Model model) {
+        model.addAttribute("artista", new Artista("cc",23,"Rock",0,"http:algo","Maria"));
+        return "formArtista";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(
+            @ModelAttribute Artista artista
+    ) {
+        LoggerFactory.getLogger(getClass()).info("Guardando artista +  " + artista);
+        // mandarlo a BD (save)
+        return "redirect:/tienda/nuevo?exito";
     }
 
 
